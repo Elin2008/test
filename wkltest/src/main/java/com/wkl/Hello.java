c7d7305a7b8d0dc410890f548c5d9f2296e4c714
Elin2008 <1367836781@qq.com>
2016-01-07 14:30:11 +0800

    test01

---
+++ wkltest/src/main/java/com/wkl/Hello.java
@@ -0,0 +1,10 @@
+package com.wkl;
+
+/**
+ * Created by crv on 2015-12-29.
+ */
+public class Hello {
+    public static void main(String[] args) {
+        System.out.printf("hello,world!");
+    }
+}
